75b3e1bf2a2e7a6466f269eaa2261deeb5dad35c
LeeNJU <lhb11@software.nju.edu.cn>
2014-07-04 11:04:08 +0800

    permuteChange

--- LeetCode/permute.cpp
+++ LeetCode/permute.cpp
@@ -5,32 +5,41 @@
 
 using namespace std;
 
-void permutation(vector<vector<int> >& vec, const vector<int>& num, deque<int>& item)
+void permutation(vector<vector<int> >& vec, const vector<int>& num, vector<int>& item, int depth)
 {
 	for (size_t i = 0; i < num.size(); ++i)
 	{
-		if (find(item.begin(), item.end(), num[i]) == item.end())
+		/*
+		bool existed = false;
+		for (size_t j = 0; j < item.size() - depth; ++j)
 		{
-			item.push_back(num[i]);
-			if (item.size() == num.size())
+			if (item[j] == num[i])
 			{
-				vec.push_back(vector<int>(item.begin(), item.end()));
-				item.pop_back();
+				existed = true;
+				break;
+			}
+		}*/
+		vector<int>::iterator iter = item.end();
+		if (find(item.begin(), iter - depth, num[i]) == iter - depth)
+		{
+			item[num.size() - depth] = num[i];
+			if (depth == 1)
+			{
+				vec.push_back(item);
 			}
 			else
 			{
-				permutation(vec, num, item);
+				permutation(vec, num, item, depth - 1);
 			}
 		}
-		item.clear();
 	}
 }
 
 vector<vector<int> > permute(vector<int>& num)
 {
 	vector<vector<int> > result;
-	deque<int> item;
-	permutation(result, num, item);
+	vector<int> item(num.size());
+	permutation(result, num, item, num.size());
 	return result;
 }
 
@@ -40,5 +49,8 @@ int main()
 	num.push_back(0);
 	num.push_back(1);
 	vector<vector<int> > vec = permute(num);
+	cout << vec.size() << endl;
+	cout << vec[0][0] << " " << vec[0][1] << endl;
+	cout << vec[1][0] << " " << vec[0][1] << endl;
 	return 0;
 }
