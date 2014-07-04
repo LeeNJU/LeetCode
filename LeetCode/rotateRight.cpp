210fc8dedbb694c40ca84c30cdced34438eb0252
LeeNJU <lhb11@software.nju.edu.cn>
2014-07-04 23:20:46 +0800

    rotate List

---
+++ LeetCode/rotateRight.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr){}
+};
+
+ListNode* rotateRight(ListNode* head, int k)
+{
+	if (head == nullptr || k <= 0)
+		return head;
+	ListNode* last = head;
+	int length = 1;
+	while (last->next)
+	{
+		++length;
+		last = last->next;
+	}
+
+	if (length == k)
+		return head;
+
+	ListNode* first = head;
+	int count = length - (k % length) - 1;
+	while (count > 0)
+	{
+		first = first->next;
+		--count;
+	}
+
+	last->next = head;
+	ListNode* result = first->next;
+	first->next = nullptr;
+	return result;
+}
