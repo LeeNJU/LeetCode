5a997af7d9b7c89417d4b16f25b661842b437e25
LeeNJU <lhb11@software.nju.edu.cn>
2014-07-04 23:18:30 +0800

    reverse Linked List 11

---
+++ LeetCode/reverseBetween.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr){}
+};
+
+ListNode* reverseBetween(ListNode* head, int m, int n)
+{
+	if (head == nullptr)
+		return nullptr;
+
+	ListNode* node = head, *p = nullptr, *q = nullptr;
+	int count = 1;
+	while (count <= n)
+	{
+		if (count == m)
+			p = node;
+		if (count == n)
+			q = node;
+		node = node->next;
+		++count;
+	}
+
+	while (p != q)
+	{
+		int swap = q->val;
+		q->val = p->val;
+		p->val = swap;
+		p = p->next;
+		ListNode* tail = p;
+
+		while (tail != q && tail->next != q)
+		{
+			tail = tail->next;
+		}
+		q = tail;
+	}
+
+	return head;
+}
