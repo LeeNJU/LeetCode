2fbcb0609c847169d2c4723acdbd080551ccae32
LeeNJU <lihaibonju@gmail.com>
2014-07-05 12:08:57 +0800

    build Tree From Preorder And Inorder

---
+++ LeetCode/buildTreeFromPreorderAndInorder.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode
+{
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL){}
+};
+
+TreeNode* build(vector<int>::iterator prefirst, vector<int>::iterator prelast,
+	vector<int>::iterator infirst, vector<int>::iterator inlast)
+{
+	if (prefirst >= prelast || infirst >= inlast)
+		return NULL;
+
+	TreeNode* root = new TreeNode(*prefirst);
+	vector<int>::iterator iter = find(infirst, inlast, root->val);
+
+	vector<int>::iterator first = prefirst;
+	advance(first, distance(infirst, iter));
+	root->left = build(++prefirst, ++first, infirst, iter);
+
+	root->right = build(first, prelast, ++iter, inlast);
+
+	return root;
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+{
+	return build(preorder.begin(), preorder.end(), inorder.begin(), inorder.end());
+}
+
