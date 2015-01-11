95acb1cc378150f08ba143eede54b21528e6b1ab
LeeNJU <lhb11@software.nju.edu.cn>
2015-01-11 21:03:17 +0800

    Twosum changes

--- LeetCode/twoSum.cpp
+++ LeetCode/twoSum.cpp
@@ -1,34 +1,31 @@
 #include<vector>
 #include<algorithm>
+#include<unordered_map>
 
-using namespace std;
+//题目描述：给定一个数组，从中间找出2个数使其加起来等于指定的一个数，例如array={2,9,33,1}找出和等于11的2个数
+//解法：用unordered_map存每个数的下标，时间复杂度为O(n)
 
-vector<int> twoSum(vector<int>& numbers, int target)
+std::vector<int> twoSum(std::vector<int>& numbers, int target)
 {
-	vector<int> number(numbers.begin(), numbers.end());
-	sort(number.begin(), number.end());
-	vector<int> result;
-	int first = 0, last = number.size() - 1;
-	while (first < last)
+	std::vector<int> result;
+	std::unordered_map<int, int> map;
+
+	for (int i = 0; i < numbers.size(); ++i)//用hash表存每个数的下标
+		map[numbers[i]] = i;
+
+	for (int i = 0; i < numbers.size(); ++i)
 	{
-		if (number[first] + number[last] < target)
-			++first;
-		else if (number[first] + number[last] > target)
-			--last;
-		else
+		std::unordered_map<int, int>::iterator iter = map.find(target - numbers[i]);
+		if (iter != map.end())//在hash表中查找元素，复杂度为常数级
 		{
+			if (iter->second == i)//找到了同一个元素，例如array={3,2,4}中找和等于6的2个数，防止重复找到3
+				continue;
+
+			result.push_back(i + 1);
+			result.push_back(map[target - numbers[i]] + 1);
 			break;
 		}
 	}
-	vector<int>::iterator iter = find(numbers.begin(), numbers.end(), number[first]);
-	result.push_back(iter - numbers.begin() + 1);
-	vector<int>::iterator iters = find(numbers.begin(), numbers.end(), number[last]);
-	if (iter == iters)//防止找到同一个下标  例如0,0,3,2 始终会找到第一个元素0
-	{
-		iters = find(++iters, numbers.end(), number[last]);
-	}
-	result.push_back(iters - numbers.begin() + 1);
-	sort(result.begin(), result.end());
 	return result;
 }
 
