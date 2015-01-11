f1d719d61d09577221f88e3d685eb077c201bf39
LeeNJU <lhb11@software.nju.edu.cn>
2015-01-11 22:28:34 +0800

    threeSum changes

--- LeetCode/threeSum.cpp
+++ LeetCode/threeSum.cpp
@@ -2,49 +2,43 @@
 #include<algorithm>
 #include <iostream>
 
-void twoSum(std::vector<std::vector<int>>& result, const std::vector<int>& num, int index, int target)
-{
-	std::vector<int> vec;
-	int first = index + 1, last = num.size() - 1;//first为index+1是为了防止同一个组合中的数字重复计算
-	while (first < last)
-	{
-		if (num[first] + num[last] > target)
-			--last;
-		else if (num[first] + num[last] < target)
-			++first;
-		else
-		{
-			vec.push_back(num[index]);
-			vec.push_back(num[first]);
-			vec.push_back(num[last]);
-			std::cout << num[index] << " " << num[first] << " " << num[last] << std::endl;
-			result.push_back(vec);
-			vec.clear();
-			++first;
-			--last;
-			while (first < last && num[first] == num[first - 1])
-				++first;
-			while (first < last && num[last] == num[last + 1])
-				--last;
-		}
-	}
-}
+//题目描述：给定一个数字，从中找出三个数，使得它们的和等于0，返回下标
+//解法：先排序，确定其中一个数，用两个头尾指针向中间逼近查找，复杂度O(n2)
+//可能会有多种结果和重复结果，都要考虑，例如array={0,0,0,0}，答案应该是0,0,0一种
 
 std::vector<std::vector<int>> threeSum(std::vector<int>& num)
 {
 	sort(num.begin(), num.end());
 	std::vector<std::vector<int>> result;
-	for (int i = 0; i < (int)num.size() - 2; ++i)//num.size返回的是无符号类型，无符号类型0减去一个数，结果不是负数，会变大
+	for (int i = 0; i < (int)num.size() - 2; ++i)//num.size() - 2,防止下标越界
 	{
-		if (i != 0 && num[i] == num[i - 1])
+		if (i > 0 && num[i] == num[i - 1])//当元素i与元素i-1相等时，跳过
 			continue;
-		twoSum(result, num, i, -num[i]);
+
+		int value = -num[i], left = i + 1, right = num.size() - 1;
+		while (left < right)//两个下标，从两头往中间逼近
+		{
+			if (left > i + 1 && num[left] == num[left - 1])//遇到重复元素时，跳过
+			{
+				++left;
+				continue;
+			}
+			if (right < num.size() - 1 && num[right] == num[right + 1])//遇到重复元素时，跳过
+			{
+				--right;
+				continue;
+			}
+			if (num[left] + num[right] > value)
+				--right;
+			else if (num[left] + num[right] < value)
+				++left;
+			else
+			{
+				result.push_back(std::vector<int> {num[i], num[left], num[right]});//找到一种结果
+				++left;
+				--right;
+			}
+		}
 	}
 	return result;
-}
-int main()
-{
-	std::vector<int> vec = {-1, 0, 1, 2, -1, -4};
-	std::cout << threeSum(vec).size() << std::endl;
-	return 0;
 }
