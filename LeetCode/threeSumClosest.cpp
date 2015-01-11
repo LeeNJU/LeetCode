f5fd5d31f9952522558242d2972761fd1e2e2faf
LeeNJU <lhb11@software.nju.edu.cn>
2015-01-11 22:57:53 +0800

    threeSum Closest

--- LeetCode/threeSumClosest.cpp
+++ LeetCode/threeSumClosest.cpp
@@ -1,36 +1,30 @@
 #include<vector>
 #include<algorithm>
 
-void twoSumClosest(std::vector<int>& num, int index, int target)
+//题目描述，给定一个数组，从中找出三个数，使它们的和与target最接近，即绝对值最小，返回三个数的和
+//解法，先排序，然后用头尾两个指针向中间逼近，开始设置绝对值差值为INT_MAX，找到绝对值最小的和
+
+int threeSumClosest(std::vector<int> &num, int target) 
 {
-	int first = index + 1, last = num.size() - 1;
-	int sum = num[index] + num[first] + num[last];
-	bool isBig = sum > target ? true : false;
-	while (first < last)
-	{
-		if (isBig && abs(sum - target) > abs(num[index] + num[first] + num[last] - target))
-		{
-			sum = num[index] + num[first] + num[last];
-			--last;
-		}
-		else if (isBig && abs(sum - target) <= abs(num[index] + num[first] + num[last] - target))
-		{
-			--last;
-		}
-		else if (!isBig && abs(sum - target) > abs(num[index] + num[first] + num[last] - target))
-		{
-
-		}
-	}
-}
-
-int threeSumClosest(std::vector<int> &num, int target) {
 	sort(num.begin(), num.end());
-	for (int i = 0; i < (int) num.size() - 2; ++i)
+	int sum = 0, diff = INT_MAX;
+	for (int i = 0; i < (int)num.size() - 2; ++i)
 	{
-		if (i != 0 && num[i] == num[i - 1])
-			continue;
-
+		int left = i + 1, right = num.size() - 1;
+		while (left < right)
+		{
+			if (abs(num[i] + num[left] + num[right] - target) < diff)//和比上一次的更接近，更新
+			{
+				sum = num[i] + num[left] + num[right];
+				diff = abs(num[i] + num[left] + num[right] - target);
+			}
+			if (num[i] + num[left] + num[right] < target)//控制下标的走向
+				++left;
+			else if (num[i] + num[left] + num[right] > target)
+				--right;
+			else
+				return sum;
+		}
 	}
-	return 0;
-}
+	return sum;
+}-3 0 1 2 
