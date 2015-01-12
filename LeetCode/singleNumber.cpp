d077b9ac98ad1db843d37c6f1e623b934a5b1d0e
LeeNJU <lhb11@software.nju.edu.cn>
2015-01-12 18:01:40 +0800

    singleNumber

--- LeetCode/singleNumber.cpp
+++ LeetCode/singleNumber.cpp
@@ -1,24 +1,11 @@
-#include<iostream>
-#include<map>
-
-using namespace std;
+//给定一个数组，数组中的元素都会出现2次，只有其中一个元素只出现1次，找出这个元素
+//解法：对每个元素进行异或操作，因为出现2次的元素进行异或之后每一位等于0，再与剩下的元素进行异或，结果不变，最终结果就是它
 
 
 int singleNumber(int A[], int n)
 {
-	map<int, int> m;
-
-	for (int i = 0; i < n; ++i)
-	{
-		++m[A[i]];
-	}
-
-	map<int, int>::iterator iter = m.begin();
-	while (iter != m.end())
-	{
-		if (iter->second == 1)
-			return iter->first;
-		++iter;
-	}
-	return 0;
+	int result = A[0];
+	for (int i = 1; i < n; ++i)
+		result ^= A[i];//注意异或操作的符号
+	return result;
 }
