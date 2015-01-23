9868fb543167fad92f99c78ba30a2f7dca73152e
LeeNJU <lhb11@software.nju.edu.cn>
2015-01-23 20:26:59 +0800

    isPalindrome

--- LeetCode/isPalindrome.cpp
+++ LeetCode/isPalindrome.cpp
@@ -1,18 +1,18 @@
-#include<iostream>
 #include<string>
 #include<cctype>
-
+//题目描述：给定一个字符串(只包含数字和字母)，判断其是否是回文串，忽略大小写
+//解法：两个头尾指针向中间逼近，其中要忽略空格的情况
 
 bool isPalindrome(std::string s)
 {
 	int first = 0, last = s.length() - 1;
 	while (first <= last)
 	{
-		if (!isalnum(s[first]))
+		if (!isalnum(s[first]))//判断是否为空格
 		{
 			++first;
 		}
-		else if (!isalnum(s[last]))
+		else if (!isalnum(s[last]))//判断是否为空格
 		{
 			--last;
 		}
