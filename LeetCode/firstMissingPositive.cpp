dfbc0e358c9f3cb80aff55a3f415fd0290c46276
LeeNJU <lhb11@software.nju.edu.cn>
2015-01-23 12:46:07 +0800

    removeElement

---
+++ LeetCode/firstMissingPositive.cpp
@@ -0,0 +1,13 @@
+#include<algorithm>
+
+int firstMissingPositive(int A [], int n)
+{
+	int index = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if (A[i] != i + 1)
+		{
+			std::swap(A[i], A[++index])
+		}
+	}
+}
