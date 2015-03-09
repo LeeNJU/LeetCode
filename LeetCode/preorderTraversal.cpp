cb3ceeb42c0b78bb205bae8ad631ee4f61b80478
LeeNJU <lihaibonju@gmail.com>
2015-03-09 20:49:54 +0800

    preOrderTraversal

--- LeetCode/preorderTraversal.cpp
+++ LeetCode/preorderTraversal.cpp
@@ -2,30 +2,28 @@
 #include<vector>
 #include<stack>
 #include"TreeNode.h"
-using namespace std;
+//题目描述：先根遍历二叉树
+//解法描述：先用栈保留根节点，然后遍历，注意先把右子节点压入栈中，再把左子节点压入栈中，因为栈是后进先出，这样下一个循环就先遍历左子节点
 
 
-vector<int> preorderTraversal(TreeNode *root) {
-	vector<int> result;
-	if (root == NULL)
+std::vector<int> preorderTraversal(TreeNode *root) {
+	std::vector<int> result;
+	if (root == nullptr)
 		return result;
 
-	stack<TreeNode*> st;
+	std::stack<TreeNode*> st;
 	TreeNode* p = root;
-	while (p != NULL || !st.empty())
+	st.push(p);//把根节点压入栈中
+	while (!st.empty())
 	{
-		if (p != NULL)
-		{
-			st.push(p);
-			result.push_back(p->val);
-			p = p->left;
-		}
-		else
-		{
-			p = st.top();
-			st.pop();
-			p = p->right;
-		}
+		p = st.top();//取出栈顶元素
+		st.pop();
+		result.push_back(p->val);
+
+		if (p->right != nullptr)//先压入右子节点
+			st.push(p->right);
+		if (p->left != nullptr)
+			st.push(p->left);
 	}
 	return result;
 }
