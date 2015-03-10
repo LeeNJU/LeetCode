46ee45af4b85d7e7e9bbfc3e1285142a08085427
LeeNJU <lihaibonju@gmail.com>
2015-03-10 20:10:20 +0800

    add

---
+++ LeetCode/convert.cpp
@@ -0,0 +1,12 @@
+#include<string>
+
+//题目描述：给定字符串PAYPALISHIRING，以如下方式解读：
+//P   A   H   N
+//A P L S I I G
+//Y   I   R, 转换成PAHNAPLSIIGYIR，即一排排连起来
+//解法描述：
+
+std::string convert(std::string s, int nRows)
+{
+	
+}
