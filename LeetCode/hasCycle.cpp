ae3fd8f3e4bb315150dc1112aa6358afecad4f8c
LeeNJU <lihaibonju@gmail.com>
2015-03-18 21:39:22 +0800

    hasCycle

--- LeetCode/hasCycle.cpp
+++ LeetCode/hasCycle.cpp
@@ -1,13 +1,11 @@
 #include"TreeNode.h"
-using namespace std;
+//题目描述：给定一个链表，判断是否有环
+//解法描述：两个指针slow和fast，slow每次前进一步，fast每次前进两步，如果二者能够相等，就表示有环
 
 
 
 bool hasCycle(ListNode* head)
 {
-	if (head == nullptr)
-		return false;
-
 	ListNode* slow = head, *fast = head;
 	while (fast && fast->next)
 	{
