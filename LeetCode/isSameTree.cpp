62e64a5324fa01b92b45f84fada91c0ee2596d30
LeeNJU <lihaibonju@gmail.com>
2015-03-20 20:54:32 +0800

    isSameTree

--- LeetCode/isSameTree.cpp
+++ LeetCode/isSameTree.cpp
@@ -1,18 +1,19 @@
 #include"TreeNode.h"
-using namespace std;
+//题目描述：给定两个二叉树，判断这两棵树是否相等
+//解法描述：递归
 
 
 
 bool isSameTree(TreeNode* p, TreeNode* q)
 {
-	if (p == nullptr && q == nullptr)
+	if (p == nullptr && q == nullptr)//同时为null
 		return true;
 
-	if (p == nullptr || q == nullptr)
+	if (p == nullptr || q == nullptr)//其中一个为null，但不同时为null
 		return false;
 	else if (p->val != q->val)
 		return false;
 	else
-		return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+		return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);//必须左子树和右子树同时相等，所以是&&
 
 }
