a2386efb0594b3d02c515ab4501e3e78d728344e
LeeNJU <lihaibonju@gmail.com>
2015-03-24 19:56:56 +0800

    removeNthFromEnd

--- LeetCode/removeNthFromEnd.cpp
+++ LeetCode/removeNthFromEnd.cpp
@@ -1,35 +1,27 @@
 #include"TreeNode.h"
-using namespace std;
-
+//题目描述：给定一个链表，要求删除倒数第n个元素
+//解法描述：添加一个哨兵节点，求出链表长度，把指针移动到要删除的元素的前一个元素，进行删除
 
 
 ListNode* removeNthFromEnd(ListNode* head, int n)
 {
-	if (head == nullptr)
-		return nullptr;
-
-	int length = 1;
-	ListNode* p = head;
-	while (p->next != nullptr)
+	int length = 0;
+	ListNode* node = head;
+	while (node)//求出链表的长度
 	{
+		node = node->next;
 		++length;
-		p = p->next;
 	}
-
-	if (length == n)
+	
+	ListNode dummy(-1);
+	dummy.next = head;
+	ListNode* p = &dummy;
+	while (length - n)//移动指针，把指针移到要删除的元素的前一个元素
 	{
-		head = head->next;
-		return head;
-	}
-
-	int count = length - n - 1;
-	p = head;
-	while (count > 0)
-	{
-		--count;
 		p = p->next;
+		--length;
 	}
 
 	p->next = p->next->next;
-	return head;
+	return dummy.next;
 }
