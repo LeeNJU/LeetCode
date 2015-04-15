a3cf7ca7afb75f2022bd78a88bc89402d4c8ecff
LeeNJU <lihaibonju@gmail.com>
2015-04-15 16:33:14 +0800

    bugs for isValideBST

--- LeetCode/isValidBST.cpp
+++ LeetCode/isValidBST.cpp
@@ -1,49 +1,20 @@
-#include<stack>
 #include<vector>
 #include"TreeNode.h"
-using namespace std;
-
-
-
-bool isValidBST(TreeNode* root)
+//题目描述：给定一个树，判断其是否是二叉树
+//解法描述：递归传入两个参数，一个是左界，一个是右界，节点的值必须在两个界的中间，同时在判断做子树和右子树时更新左右界。
+//bugs  此方法行不通  最好是中序
+bool isValidBST(TreeNode* root, int lower, int upper)//注意必须是long，因为当root的值为INT_MAX时为出错
 {
 	if (root == nullptr)
 		return true;
-
-	if (root->left == nullptr && root->right == nullptr)
-		return true;
-
-	stack<TreeNode*> s;
-	vector<int> result;
-	TreeNode* p = root;
-
-	while (p != nullptr)
-	{
-		s.push(p);
-		p = p->left;
-	}
-
-	while (!s.empty())
-	{
-		TreeNode* q = s.top();
-		s.pop();
-		result.push_back(q->val);
-
-		if (q->right != nullptr)
-		{
-			TreeNode* node = q->right;
-			while (node != nullptr)
-			{
-				s.push(node);
-				node = node->left;
-			}
-		}
-	}
-
-	for (size_t i = 0; i < result.size() - 1; i++)
-	if (result[i] >= result[i + 1])
+		
+	if (root->val <= lower || root->val >= upper)
 		return false;
 
-	return true;
+	return root->val > lower && root->val < upper && isValidBST(root->left, lower, root->val) && isValidBST(root->right, root->val, upper);
+}
 
+bool isValidBST(TreeNode* root)
+{
+	return isValidBST(root, INT_MIN, INT_MAX);
 }
