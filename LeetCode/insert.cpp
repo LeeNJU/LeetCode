b246e50ce7e6fa282f508fde4b24588d93b29714
LeeNJU <lihaibonju@gmail.com>
2015-04-28 16:38:29 +0800

    insert with bugs

--- LeetCode/insert.cpp
+++ LeetCode/insert.cpp
@@ -1,54 +1,34 @@
-#include<iostream>
+#include<algorithm>
 #include<vector>
 #include"TreeNode.h"
+//题目描述：给定一组已经排好序的数字区间，向其中插入一个数字区间，如果有必要进行区间合并，例如给定[1,2],[3,5],[6,7],[8,10],[12,16]，插入[4,9]，
+//结果为[1,2],[3,10],[12,16]
+//解法描述:遍历数字区间，判断newInterval与当前区间的关系，如果与当前区间有重合部分就进行合并，再删除当前区间，再继续判断新的newInterval与区间的关系
 
 
+//bugs
 std::vector<Interval> insert(std::vector<Interval> &intervals, Interval newInterval)
 {
-	int first = -1, last = -1; //记录插入的位置，first为偶数说明插入的位置为一个区间之内，first为奇数，说明插入的位置为区间之间，last同理
-	for (size_t i = 0; i < intervals.size(); ++i)
+	std::vector<Interval>::iterator it = intervals.begin();
+	while (it != intervals.end()) 
 	{
-		if (intervals[i].start <= newInterval.start && intervals[i].end >= newInterval.start)
-			first = i * 2;
-		if (newInterval.start > intervals[i].end)
-			first = i * 2 + 1;
-		if (intervals[i].start <= newInterval.end && intervals[i].end >= newInterval.end)
-			last = i * 2;
-		if (newInterval.end > intervals[i].end)
-			last = i * 2 + 1;
-	}
-
-	if (last == -1)
-	{
-		intervals.insert(intervals.begin(), newInterval);
-	}
-	else if (first == -1)
-	{
-		int index = last / 2;
-		int temp = intervals[index].end;
-		intervals.erase(intervals.begin(), intervals.begin() + index + 1);
-		if (last % 2 == 0)
-			newInterval.end = temp;
-		intervals.insert(intervals.begin(), newInterval);
-	}
-	else if (first > intervals.size() * 2)
-	{
-		intervals.push_back(newInterval);
-	}
-	else
-	{
-		if (first % 2 == 0 && last % 2 == 1)//调整插入的interval的值
-			newInterval.start = intervals[first / 2].start;
-		if (first % 2 == 1 && last % 2 == 0)
-			newInterval.end = intervals[last / 2].end;
-		if (first % 2 == 0 && last % 2 == 0)
+		if (newInterval.end < it->start) //newInterval在当前区间的前面
 		{
-			newInterval.start = intervals[first / 2].start;
-			newInterval.end = intervals[last / 2].end;
+			intervals.insert(it, newInterval);
+			return intervals;
 		}
-			
-		intervals.erase(intervals.begin() + (first + 1) / 2, intervals.begin() + last / 2 + 1);
-		intervals.insert(intervals.begin() + (first + 1) / 2, newInterval);
+		else if (newInterval.start > it->end) //newInterval区间在当前区间的后面
+		{
+			it++;
+		}
+		else//newInterval与当前区间有重复，把newInterval和当前区间合并，删除当前区间
+		{
+			newInterval.start = std::min(newInterval.start, it->start);
+			newInterval.end = std::max(newInterval.end, it->end);
+			it = intervals.erase(it);
+		}
 	}
+	intervals.insert(intervals.end(), newInterval);//插入最后
+
 	return intervals;
 }
