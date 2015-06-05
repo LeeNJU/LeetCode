09d63f336077a57aef349920e6110aa5b4648a43
LeeNJU <lihaibonju@gmail.com>
2015-06-05 20:31:05 +0800

    findMin2

---
+++ LeetCode/Binary Tree Right Side View.cpp
@@ -0,0 +1,38 @@
+#include"TreeNode.h"
+#include<vector>
+#include<queue>
+
+//题目描述：给定一个二叉树，假设站在这颗树的右边，找到你能看到的元素，即求出树每一层的最右边的元素
+//解法描述:层次遍历二叉树，用null作分隔符，用一个变量记录每一层的元素，当遇到null分隔符的时候，这个变量就是这一层的最右边的元素
+
+std::vector<int> rightSideView(TreeNode* root)
+{
+	std::vector<int> result;
+	if (root == nullptr)
+		return result;
+
+	std::queue<TreeNode*> q;
+	q.push(root);
+	q.push(nullptr);//null为层与层之间的分隔符
+	int value = 0;//value记录这一层最右边的元素
+	while (!q.empty())
+	{
+		TreeNode* p = q.front();
+		q.pop();
+		if (p != nullptr)
+		{
+			value = p->val;
+			if (p->left)//压入左子树和右子树
+				q.push(p->left);
+			if (p->right)
+				q.push(p->right);
+		}
+		else
+		{
+			result.push_back(value);
+			if (!q.empty())
+				q.push(nullptr);//压入null作为层分隔符
+		}
+	}
+	return result;
+}
--- LeetCode/findMin.cpp
+++ LeetCode/findMin.cpp
@@ -1,20 +1,35 @@
 #include <vector>
 
-//假设数组中没有重复的元素
+//题目描述：给定一个数组，该数组之前已排好序，但是在某一个点被调换了位置，例如4 5 6 7 0 1 2，找到其中的最小元素，假设数组中不包含重复元素
+//解法描述：二分查找
+
 int findMin(std::vector<int> &num)//将中间的元素与最右边的元素比较，用二分法缩小范围，复杂度为O(lgn)
 {
 	int left = 0, right = num.size() - 1, middle = (left + right) / 2;
 	while (left < right)
 	{
 		if (num[middle] > num[right])
-		{
 			left = middle + 1;
-		}
 		else
-		{
 			right = middle;
-		}
 		middle = (left + right) / 2;
 	}
 	return num[left];
+}
+
+//版本二：假设数组中包含重复元素
+int findMin2(std::vector<int>& nums)
+{
+	int left = 0, right = nums.size() - 1, middle = 0;
+	while (left < right)
+	{
+		middle = (left + right) / 2;
+		if (nums[left] == nums[right])//两遍两个元素相等，只需要移动left或right一个指针就可以了，不能两个都移动,例如[1,2,1]
+			++left;
+		else if (nums[middle] > nums[right])
+			left = middle + 1;
+		else
+			right = middle;
+	}
+	return nums[left];
 }
