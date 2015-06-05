1a3e89896b5bbb1095d048cea1f2f58d40f606a1
LeeNJU <lihaibonju@gmail.com>
2015-06-05 19:23:12 +0800

    isomorphic string

---
+++ LeetCode/Isomorphic Strings.cpp
@@ -0,0 +1,30 @@
+#include<string>
+#include<unordered_map>
+
+//题目描述：给定两个字符串，判断他们是不是isomorphic的
+//解法描述：用hashtable保存s到t的映射关系，检查s是不是映射到t，再检查t是不是映射到s
+
+bool isIsomorphic(std::string s, std::string t)
+{
+	if (s.length() != t.length())
+		return false;
+
+	std::unordered_map<char, char> mp;
+	for (int i = 0; i < s.length(); ++i) 
+	{
+		if (mp.find(s[i]) == mp.end()) //保存是s[i]到t[i]的映射
+			mp[s[i]] = t[i];
+		else if (mp[s[i]] != t[i]) //判断当前s[i]是不是映射到t[i]
+			return false;
+	}
+
+	mp.clear();//反过来检查t是不是映射到s，因为可能s中的多个元素映射到t中的同一个元素，所以要检查t是不是映射到s
+	for (int i = 0; i < s.length(); ++i) //
+	{
+		if (mp.find(t[i]) == mp.end()) 
+			mp[t[i]] = s[i];
+		else if (mp[t[i]] != s[i]) 
+			return false;
+	}
+	return true;
+}
---
+++ LeetCode/Minimum Size Subarray Sum.cpp
@@ -0,0 +1,26 @@
+#include<vector>
+#include<algorithm>
+//题目描述：给定一个正整数数组和一个正整数s，从数组中找到和大于等于s的最小子数组的长度,例如[2,3,1,2,4,3] and s = 7，最小长度为[4,3]
+//解法描述：用left和right两个指针维护这个子数组，sum为子数组的和，向右移动right，直到sum大于等于s，然后循环去除子数组的左边部分
+
+int minSubArrayLen(int s, std::vector<int>& nums)
+{
+	int min_length = nums.size() + 1, sum = 0;//把min_length设成一个比较大的值，方便下面比较
+	int left = 0, right = 0;
+	while (right < nums.size())
+	{
+		sum += nums[right];
+		if (sum >= s)//sum一旦大于s，表明至少存在一个解
+		{
+			min_length = std::min(min_length, right - left + 1);
+			while (sum - nums[left] >= s)//去除左边多余的元素，left向右移动
+			{
+				sum -= nums[left];
+				++left;
+			}
+			min_length = std::min(min_length, right - left + 1);
+		}
+		++right;
+	}
+	return min_length == nums.size() + 1 ? 0 : min_length;//如果min_length为初始值，说明无解，返回0
+}
