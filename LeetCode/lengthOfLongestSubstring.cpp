9334387627745cc3b924aa0177ab95963618c035
LeeNJU <lhb11@software.nju.edu.cn>
2015-06-08 00:25:42 +0800

    length

--- LeetCode/lengthOfLongestSubstring.cpp
+++ LeetCode/lengthOfLongestSubstring.cpp
@@ -3,30 +3,24 @@
 #include<algorithm>
 
 //题目描述：给定一个字符串，找出其中最大的没有重复字符的子串的长度。"abcabcbb"的是abc,长度为3，"bbbbb"的是b，长度为1
-//解法描述：用两个指针，分别指向当前子串，相当于窗口，用hashtable表示子串中出现过的元素，如果当前元素没有重复，j加1，如果重复了，重复出现的位置为i，那么
-//左边指针的位置为i+1，同时要删去hashtable中的部分元素，只保留i+1到j的元素   bugs
+//解法描述：用两个指针，分别指向当前子串，相当于窗口，遍历字符串，并检查在[i,j]区间里是否有重复元素
 
 int lengthOfLongestSubstring(std::string s)
 {
 	int result = 0;
-	std::unordered_map<char, int> m;
-
-	for (int i = 0, j = 0; j < s.length();)
+	for (int i = 0, j = 0; i <= j && j < s.length(); ++j)
 	{
-		std::unordered_map<char, int>::iterator iter = m.find(s[j]);
-		if (iter == m.end())//元素没有重复
+		int index = i;
+		while (index < j)
 		{
-			m[s[j]] = j;
-			++j;
+			if (s[index] == s[j])
+			{
+				i = index + 1;
+				break;
+			}
+			++index;
 		}
-		else
-		{
-			int index = iter->second + 1;
-			m.erase(m.begin(), );
-			i = index;
-		}
-		result = result > j - i ? result : j - i;
+		result = result >= j - i + 1 ? result : j - i + 1;
 	}
-
 	return result;
 }
