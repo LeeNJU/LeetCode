287ff952aa189fe643734475189e8c28030d6b7a
LeeNJU <lihaibonju@gmail.com>
2015-06-13 20:55:11 +0800

    Reverse Linked List II

--- LeetCode/reverseBetween.cpp
+++ LeetCode/reverseBetween.cpp
@@ -1,40 +1,26 @@
 #include"TreeNode.h"
 
-using namespace std;
-
-
-
+//题目描述：给定一个链表，把从第m到n个节点进行反转
+//解法描述：用一个哨兵节点，一遍扫描，依次把要反转的元素放到前面，
 ListNode* reverseBetween(ListNode* head, int m, int n)
 {
-	if (head == nullptr)
-		return nullptr;
+	ListNode dummy(-1);
 
-	ListNode* node = head, *p = nullptr, *q = nullptr;
-	int count = 1;
-	while (count <= n)
+	dummy.next = head;
+	ListNode *prev_m, *prev = &dummy; //prev_m指向第m个节点的前一个节点，prev指向第m个节点，head指向第m个节点下一个节点
+	for (int i = 1; i <= n; ++i) 
 	{
-		if (count == m)
-			p = node;
-		if (count == n)
-			q = node;
-		node = node->next;
-		++count;
-	}
-
-	while (p != q)
-	{
-		int swap = q->val;
-		q->val = p->val;
-		p->val = swap;
-		p = p->next;
-		ListNode* tail = p;
-
-		while (tail != q && tail->next != q)
-		{
-			tail = tail->next;
+		if (i == m) 
+			prev_m = prev;
+		if (i > m && i <= n) // prev_m 和prev 静止不动
+		{ 
+			prev->next = head->next;
+			head->next = prev_m->next;
+			prev_m->next = head;
+			head = prev;
 		}
-		q = tail;
+		prev = head;
+		head = head->next;
 	}
-
-	return head;
+	return dummy.next;
 }
