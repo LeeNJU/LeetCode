f7f702b4021dc05e17d0ddd9e8d37c79a43cfa24
LeeNJU <lihaibonju@gmail.com>
2015-06-16 14:36:00 +0800

    Fraction Occuring Number

--- LeetCode/Fraction to Recurring Decimal.cpp
+++ LeetCode/Fraction to Recurring Decimal.cpp
@@ -6,20 +6,23 @@
 
 std::string fractionToDecimal(int numerator, int denominator)
 {
-	if (numerator == 0)
+	long long num = static_cast<long long>(numerator); //强制类型转换,这是为了防止出现溢出的情况，例如-2147483647，-1
+	long long denomin = static_cast<long long>(denominator);
+
+	if (num == 0) //处理特殊情况
 		return "0";
-	if (denominator == 1)
-		return std::to_string(numerator);
+	if (denomin == 1)
+		return std::to_string(num);
+	if (denomin == -1)
+		return std::to_string(-num);
 
 	std::string result;
-	if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0))//添加负号
+	if ((num < 0 && denomin > 0) || (num > 0 && denomin < 0))//添加负号
 		result += "-";
 
-	long long num = static_cast<long long>(numerator);
-	long long denomin = static_cast<long long>(denominator);
 	num = abs(num);//全部转换成正数
 	denomin = abs(denomin);
-	int val = num / denomin;
+	long long val = num / denomin;
 	result += std::to_string(val); //先求整数部分
 	if (val * denomin == num)//刚好整除 直接返回
 		return result;
@@ -28,11 +31,11 @@ std::string fractionToDecimal(int numerator, int denominator)
 	num = (num - val * denomin) * 10;
 	int indexOfPoint = result.size() - 1;  //记录小数点的位置
 
-	std::vector<int> vec; //作为判断是否循环的标准，出现循环是numerator重复去除denominator
+	std::vector<long long> vec; //作为判断是否循环的标准，出现循环是numerator重复去除denominator，而不是余数重复出现，例如1,1000000
 	while (num)
 	{
-		int value = num / denomin;
-		std::vector<int>::iterator position = find(vec.begin(), vec.end(), num); //在小数点之后的部分进行查找，是否由重复数字出现
+		long long value = num / denomin;
+		std::vector<long long>::iterator position = find(vec.begin(), vec.end(), num); //在小数点之后的部分进行查找，是否由重复数字出现
 		if (position != vec.end()) //有重复数字出现，插入（和）
 		{
 			result.insert(result.begin() + indexOfPoint + 1 + (position - vec.begin()), '(');
