1bf327cd521bce83eb47d4c553ad4fb7ae930825
LeeNJU <lhb11@software.nju.edu.cn>
2015-08-24 15:52:45 -0400

    combine

--- LeetCode/combine.cpp
+++ LeetCode/combine.cpp
@@ -1,31 +1,25 @@
-#include <iostream>
 #include <vector>
-
-using namespace std;
-
-vector<vector<int>> combine(int n, int k)
+//题目描述：给定一个值n和k，n表示1到n个数字，求出1到n中大小为k的组合，例如n = 4 and k = 2, 解为[[2, 4],[3, 4],
+//         [2, 3],[1, 2],[1, 3],[1, 4],]
+//解法描述：递归
+void dfs(std::vector<std::vector<int>>& result, std::vector<int> intermediate, int index, int k, int n)
 {
-	vector<vector<int>> result;
-	vector<int> vec(k, 0);
-	for (int i = 1; i <= n - k + 1; ++i)
+	if (intermediate.size() == k)
 	{
-		vec[0] = i;
-		//combinations(n, k, result, vec, i + 1, 1);
-	}
-	return result;
-}
-
-void combinations(int n, int k, vector<vector<int>>& result, vector<int> vec, int index, int depth)
-{
-	if (depth == k)
-	{
-		result.push_back(vec);
+		result.push_back(intermediate);
 		return;
 	}
 
 	for (int i = index; i <= n; ++i)
 	{
-		vec[depth] = i;
-		combinations(n, k, result, vec, i + 1, depth + 1);
+		intermediate.push_back(i);
+		dfs(result, intermediate, i + 1, k, n);
+		intermediate.pop_back();
 	}
+}
+std::vector<std::vector<int>> combine(int n, int k) {
+	std::vector<std::vector<int>> result;
+	std::vector<int> t;
+	dfs(result, t, 1, k, n);
+	return result;
 }
