3d076b081f36ad1fc5cf263cf42cc5ff91a962f2
LeeNJU <lhb11@software.nju.edu.cn>
2015-09-02 16:14:12 -0400

    swap nodes

--- LeetCode/swapPairs.cpp
+++ LeetCode/swapPairs.cpp
@@ -1,7 +1,6 @@
 #include"TreeNode.h"
-using namespace std;
-
-
+//题目描述：给定一个链表，每相邻两个元素进行交换，例如1->2->3->4，变成2->1->4->3
+//解法描述：每两个节点一次进行交换即可
 
 ListNode *swapPairs(ListNode *head) 
 {
@@ -18,11 +17,8 @@ ListNode *swapPairs(ListNode *head)
 
 		ListNode* next = p->next;//保存p下一个元素
 		if (p->next != nullptr && p->next->next != nullptr)//检查下两个元素的情况
-		{
 			p->next = p->next->next;
-		}
 		p = next;
 	}
-
 	return head;
 }
