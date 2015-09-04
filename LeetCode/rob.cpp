8b333a6b4730e729af8a198db36fed07071718b5
LeeNJU <lhb11@software.nju.edu.cn>
2015-09-04 13:29:28 -0400

    rob2

--- LeetCode/rob.cpp
+++ LeetCode/rob.cpp
@@ -15,3 +15,26 @@ int rob(std::vector<int>& nums)
 	}
 	return dp.back();
 }
+
+//变种2：假设房子的排列是首尾相连的，即第一个房子和最后一个房子是相邻的，不能同时抢劫
+//解法描述：由于第一家和最后一家不能同时抢，那就分别把第一家和最后一家去掉，分别算出能够抢的最大值，然后二者去最大
+int rob(std::vector<int> &nums, int left, int right)
+{
+	if (right - left <= 1) 
+		return nums[left];
+	
+	std::vector<int> dp(right, 0);
+	dp[left] = nums[left];
+	dp[left + 1] = std::max(nums[left], nums[left + 1]);
+	for (int i = left + 2; i < right; ++i) 
+	{
+		dp[i] = std::max(nums[i] + dp[i - 2], dp[i - 1]);
+	}
+	return dp.back();
+}
+int rob2(std::vector<int>& nums) 
+{
+	if (nums.size() <= 1) 
+		return nums.empty() ? 0 : nums[0];
+	return std::max(rob(nums, 0, nums.size() - 1), rob(nums, 1, nums.size()));
+}
