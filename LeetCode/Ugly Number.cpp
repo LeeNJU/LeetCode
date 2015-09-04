bf8687c411d341b62f58ebd95ce280bf5df1a2df
LeeNJU <lhb11@software.nju.edu.cn>
2015-09-04 16:24:09 -0400

    ugly number

--- LeetCode/Ugly Number.cpp
+++ LeetCode/Ugly Number.cpp
@@ -1,6 +1,9 @@
+#include<vector>
+#include<algorithm>
 //题目描述：ugly number是指只能被2，3和5整除的数，判断一个数是不是ugly number，1是ugly number
 //解法描述：不停的用2，3和5去除，如果发现除了2，3和5的约数，就不是ugly number
-bool isUgly(int num) {
+bool isUgly(int num) 
+{
 	if (num <= 0)
 		return false;
 
@@ -16,4 +19,25 @@ bool isUgly(int num) {
 			return false;
 	}
 	return true;
+}
+
+//变种2：求出第n个ugly number
+//解法描述：找出生成ugly number的规律，一个ugly number都是由一个较小的ugly number乘以2，3或5得到，index2，index3和
+//         index5用来表示前一个较小的ugly number
+int nthUglyNumber(int n) 
+{
+	std::vector<int> result(n, 0);
+	int index2 = 0, index3 = 0, index5 = 0;
+	result[0] = 1;
+	for (int i = 1; i < n; i++)
+	{
+		result[i] = std::min(std::min(result[index2] * 2, result[index3] * 3), result[index5] * 5);
+		if (result[i] == result[index2] * 2) 
+			index2++;
+		if (result[i] == result[index3] * 3) 
+			index3++;
+		if (result[i] == result[index5] * 5) 
+			index5++;
+	}
+	return result[n - 1];
 }
