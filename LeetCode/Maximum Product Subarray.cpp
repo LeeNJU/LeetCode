e16e0dad0fb4fdf6c18dfc30b85c9bf182220a9f
LeeNJU <lhb11@software.nju.edu.cn>
2015-10-24 23:18:54 -0400

    product

--- LeetCode/Maximum Product Subarray.cpp
+++ LeetCode/Maximum Product Subarray.cpp
@@ -1,7 +1,7 @@
 #include<vector>
 #include<algorithm>
 //题目描述：给定一个数组，求出最大子数组的乘积
-//解法描述：
+//解法描述：最大值可能由负数乘以负数得到,维护两个局部变量，max_local和min_local表示局部的最大值和最小值
 int maxProduct(std::vector<int>& nums) 
 {
 	if (nums.empty())
@@ -12,7 +12,7 @@ int maxProduct(std::vector<int>& nums)
 	int min_local = nums[0];
 	for (int i = 1; i < nums.size(); ++i)
 	{
-		int t = max_local;
+		int t = max_local;//注意这里要保存max_local的值，下面一行代码会改变max_local的值
 		max_local = std::max(std::max(nums[i] * max_local, nums[i] * min_local), nums[i]);
 		min_local = std::min(std::min(nums[i] * min_local, nums[i] * t), nums[i]);
 		result = std::max(result, max_local);
--- LeetCode/Word Search II.cpp
+++ LeetCode/Word Search II.cpp
@@ -1,28 +1,26 @@
 #include<vector>
 #include<string>
 
-bool existed(std::vector<std::vector<bool>> visited, const std::vector<std::vector<char>>& board, std::string& word, int index, int i, int j)
+bool existed(std::vector<std::vector<char>>& board, std::vector<std::vector<bool>>& visited, std::string word, int index, int i, int j)
 {
 	if (index == word.size())
 		return true;
 	if (visited[i][j])
 		return false;
-
-	visited[i][j] = true;
 	if (word[index] != board[i][j])
 		return false;
 
-	bool exist = false;
-	if (!exist && (i + 1) < board.size())
-		exist = existed(visited, board, word, index + 1, i + 1, j);
-	if (!exist && (i - 1) >= 0)
-		exist = existed(visited, board, word, index + 1, i - 1, j);
-	if (!exist && (j + 1) < board[0].size())
-		exist = existed(visited, board, word, index + 1, i, j + 1);
-	if (!exist && (j - 1) >= 0)
-		exist = existed(visited, board, word, index + 1, i, j - 1);
-	return exist;
-
+	visited[i][j] = true;
+	bool done = false;
+	if (!done && i + 1 < board.size())
+		done = existed(board, visited, word, index + 1, i + 1, j);
+	if (!done && j + 1 < board[0].size())
+		done = existed(board, visited, word, index + 1, i, j + 1);
+	if (!done && i - 1 >= 0)
+		done = existed(board, visited, word, index + 1, i - 1, j);
+	if (!done && j - 1 >= 0)
+		done = existed(board, visited, word, index + 1, i, j - 1);
+	return done;
 }
 
 std::vector<std::string> findWords(std::vector<std::vector<char>>& board, std::vector<std::string>& words) 
@@ -31,8 +29,18 @@ std::vector<std::string> findWords(std::vector<std::vector<char>>& board, std::v
 	std::vector<std::vector<bool>> visited(board.size(), std::vector<bool>(board[0].size(), false));
 	for (int i = 0; i < words.size(); ++i)
 	{
-		if (existed(visited, board, words[i], 0, 0, 0))
-			result.push_back(words[i]);
+		for (int j = 0; j < board.size(); ++j)
+		{
+			for (int k = 0; k < board[0].size(); ++k)
+			{
+				std::vector<std::vector<bool>> visited(board.size(), std::vector<bool>(board[0].size(),false));
+				if (board[j][k] == words[i][0])
+				{
+					if (existed(board, visited, words[i], 0, j, k))
+						result.push_back(words[i]);
+				}
+			}
+		}
 	}
 	return result;
 }
