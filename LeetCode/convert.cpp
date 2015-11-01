cea626b010c8968b0f73f6d632a69ea221007aac
LeeNJU <lhb11@software.nju.edu.cn>
2015-10-31 20:34:51 -0400

    convert

--- LeetCode/convert.cpp
+++ LeetCode/convert.cpp
@@ -4,9 +4,27 @@
 //P   A   H   N
 //A P L S I I G
 //Y   I   R, 转换成PAHNAPLSIIGYIR，即一排排连起来
-//解法描述：
+//解法描述：A到L的距离是固定的，先算A到P的距离2 * (n - 1 - i)，再算P到L的距离2 * i
 
-std::string convert(std::string s, int nRows)
-{
-	
+std::string converts(std::string s, int numRows) {
+	if (numRows <= 1)
+		return s;
+
+	std::string str = "";
+	int index = 0;
+	for (int i = 0; i < numRows && i < s.size(); ++i)
+	{
+		index = i;
+		str += s[index];
+		while (index < s.size())
+		{
+			index += (numRows - 1 - i) * 2;
+			if ((numRows - 1 - i) * 2 != 0 && index < s.size())
+				str += s[index];
+			index += 2 * i;
+			if (2 * i != 0 && index < s.size())
+				str += s[index];
+		}
+	}
+	return str;
 }
