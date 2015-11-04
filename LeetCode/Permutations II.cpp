c87ed7aea46b0e64559f481d6419196e2b03360c
LeeNJU <lhb11@software.nju.edu.cn>
2015-11-04 11:43:14 -0500

    permutation

--- LeetCode/Permutations II.cpp
+++ LeetCode/Permutations II.cpp
@@ -2,26 +2,27 @@
 #include<algorithm>
 
 //题目描述：给定一个整数数组（包含重复元素），返回这些数字所有的组合
-//解法描述：递归，先排序，遇到重复元素就跳过，非重复元素就把它放到当前序列的第一位，把该元素加入到perm中，然后递归，递归完了要排序
+//解法描述：递归，先排序，用一个visited数组保存元素是否被保存过，遇到重复元素就跳过，
 
-void dfs(std::vector<int>& nums, int index, std::vector<int> &perm, std::vector<std::vector<int> > &result)
+void dfs(std::vector<int>& num, std::vector<bool>& visited, std::vector<int>& solution, std::vector<std::vector<int>>& result)
 {
-	int size = nums.size();
-	if (size == index)  //找到一个解
-		result.push_back(perm);
-	else 
+	if (solution.size() == num.size())//找到一个解
 	{
-		for (int i = index; i < size; ++i) //从index开始迭代 
+		result.push_back(solution);
+		return;
+	}
+	for (int i = 0; i< num.size(); i++)//每次都是从0开始遍历
+	{
+		if (visited[i] == false)//元素没有被访问过才加入到solution中
 		{
-			if ((i > index) && (nums[i] == nums[index])) //有重复元素，跳过
-				continue;
-			else 
-				std::swap(nums[index], nums[i]); //不是重复元素，i和index交换，把当前元素放到第一位
-			perm.push_back(nums[index]);
-			dfs(nums, index + 1, perm, result);
-			perm.pop_back();
+			if (i > 0 && num[i] == num[i - 1] && visited[i - 1] == false)//注意这里visited[i - 1]必须是false才能
+				continue;                                                //continue
+			visited[i] = true;
+			solution.push_back(num[i]);
+			dfs(num, visited, solution, result);
+			solution.pop_back();
+			visited[i] = false;
 		}
-		sort(nums.begin() + index, nums.end()); //前面可能nums的顺序，进行恢复
 	}
 }
 
@@ -29,7 +30,8 @@ std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums)
 {
 	std::vector<std::vector<int> > result;
 	std::vector<int> perm;
+	std::vector<bool> visited(nums.size(), 0);
     std::sort(nums.begin(), nums.end());  //先进行排序，方便去重
-	dfs(nums, 0, perm, result);
+	dfs(nums, visited, perm, result);
     return result;
 }
--- LeetCode/Permutations.cpp
+++ LeetCode/Permutations.cpp
@@ -1,9 +1,9 @@
 #include<vector>
 #include<algorithm>
 //题目描述：给定一组数组，不包含重复元素，返回这组数组所有的组合情况。
-//解法描述：递归回溯
+//解法描述：递归回溯,用一个visited数组保存元素是否被访问过
 
-void dfs(std::vector<std::vector<int>>& result, std::vector<int> temp, const std::vector<int>& num)
+void dfs(std::vector<std::vector<int>>& result, std::vector<int> temp, std::vector<bool>& visited, const std::vector<int>& num)
 {
 	if (temp.size() == num.size())
 	{
@@ -13,11 +13,13 @@ void dfs(std::vector<std::vector<int>>& result, std::vector<int> temp, const std
 
 	for (int i = 0; i < num.size(); ++i)//依次扫描，每一层递归只负责一个元素，所以要pop_back
 	{
-		if (std::find(temp.begin(), temp.end(), num[i]) == temp.end())
+		if (!visited[i])
 		{
+			visited[i] = true;
 			temp.push_back(num[i]);
-			dfs(result, temp, num);
+			dfs(result, temp, visited,num);
 			temp.pop_back();//一层递归只负责一个元素
+			visited[i] = false;
 		}
 	}
 }
@@ -26,6 +28,7 @@ std::vector<std::vector<int> > permute(std::vector<int>& num)
 {
 	std::vector<std::vector<int> > result;
 	std::vector<int> temp;
-	dfs(result, temp, num);
+	std::vector<bool> visited(num.size(), false);
+	dfs(result, temp, visited, num);
 	return result;
 }
