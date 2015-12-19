e473832d2309b628b26ab781ee2967fe7f023e8b
LeeNJU <lhb11@software.nju.edu.cn>
2015-12-18 22:48:06 -0500

    Maximum Product of Word Lengths

---
+++ LeetCode/Maximum Product of Word Lengths.cpp
@@ -0,0 +1,27 @@
+#include<string>
+#include<vector>
+//题目描述:给定一个字符串数组，找到两个字符串使得它们长度的乘积最大，并且这两个字符串之间没有公共字符
+//解法描述:把每一个字符串转换成int数字，因为只有26个字母，可以用int值代替，然后进行与操作，如果没有公共字符，与操作结果是0
+int maxProduct(std::vector<std::string>& words)
+{
+	std::vector<int> vec(words.size(), 0);
+	for (int i = 0; i < words.size(); ++i)
+	{
+		for (int j = 0; j < words[i].length(); ++j)
+		{
+			vec[i] |= (1 << (words[i][j] - 'a')); //转换成int值
+		}
+	}
+
+
+	int result = 0;
+	for (int i = 0; i < words.size(); ++i)
+	{
+		for (int j = i + 1; j < words.size(); ++j)
+		{
+			if (!(vec[i] & vec[j]) && words[i].length() * words[j].length() > result)
+				result = words[i].length() * words[j].length();
+		}
+	}
+	return result;
+}
