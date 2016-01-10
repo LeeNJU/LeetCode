901344263bf53ff32a2ba47665265b1acc6b811a
LeeNJU <lhb11@software.nju.edu.cn>
2016-01-10 14:16:10 -0500

    Flip Game II

--- LeetCode/Flip Game.cpp
+++ LeetCode/Flip Game.cpp
@@ -16,3 +16,16 @@ std::vector<std::string> generatePossibleNextMoves(std::string s)
 	}
 	return moves;
 }
+
+//version 2
+//题目描述:给定一个字符串只包含+和-，两个人轮流把连续的+变成-，经过变换后，如果没有连续的++，那么该人获胜，判断该人是否获胜
+//解法描述:递归，遍历所有的解
+bool canWin(std::string s) 
+{
+	for (int i = -1; (i = s.find("++", i + 1)) >= 0;)//找到一个连续的++
+	{
+		if (!canWin(s.substr(0, i) + '-' + s.substr(i + 2)))//把连续的++变成-，然后递归判断
+			return true;
+	}
+	return false;
+}
