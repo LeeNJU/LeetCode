274c42548bcf47baef42e59c3cdd372132fc379d
LeeNJU <lhb11@software.nju.edu.cn>
2016-01-10 23:54:29 -0500

    two sum

--- LeetCode/twoSum.cpp
+++ LeetCode/twoSum.cpp
@@ -29,3 +29,51 @@ std::vector<int> twoSum(std::vector<int>& numbers, int target)
 	return result;
 }
 
+
+//version 2
+//数组已排好序，找两个元素，使得它们的和等于target
+std::vector<int> twoSum(std::vector<int> &numbers, int target)
+{
+	std::vector<int> result(2,0);
+	int left = 0, right = numbers.size() - 1;
+	while (left < right)
+	{
+		if (numbers[left] + numbers[right] == target)
+		{
+			result[0] = left + 1;
+			result[1] = right + 1;
+			return result;
+		}
+		else if (numbers[left] + numbers[right] < target)
+			++left;
+		else
+			--right;
+	}
+	return result;
+}
+
+//version 3
+//设计一个类，有两个操作，add和find，add添加一个数，find找到两个数，使得它们的和等于给定的参数
+//解法描述:用一个map保存每个元素以及出现的次数(因为可能有重复元素)，然后利用map进行查找
+class TwoSum 
+{
+private:
+	std::unordered_map<int, int> elemCount;
+
+public:
+	void add(int number) 
+	{
+		elemCount[number]++;
+	}
+
+	bool find(int value) 
+	{
+		for (auto elem : elemCount) 
+		{
+			if ((elem.first < value - elem.first && elemCount.count(value - elem.first)) ||
+				(elem.first == value - elem.first && elem.second > 1))
+				return true;
+		}
+		return false;
+	}
+};
