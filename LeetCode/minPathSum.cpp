9fbf0c441c5af1dee2a562829b182e6a01f9b119
LeeNJU <lhb11@software.nju.edu.cn>
2016-01-28 19:49:04 -0500

    delete

--- LeetCode/minPathSum.cpp
+++ LeetCode/minPathSum.cpp
@@ -1,6 +1,7 @@
 #include<vector>
 #include<algorithm>
-//题目描述：给定一个m*n的二位数组，其中每个值非负，现在找到从左上到右下的一条路径，使得路径和最小，每次只能向右或向下移动
+//题目描述：给定一个m*n的二位数组，其中每个值非负，现在找到从左上到右下的一条路径，使得路径和最小，每次只能向
+//         右或向下移动
 //解法描述：动态规划 转移方程为dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
 
 int minPathSum(std::vector<std::vector<int> > &grid)
@@ -19,9 +20,7 @@ int minPathSum(std::vector<std::vector<int> > &grid)
 	for (int i = 1; i < row; ++i)
 	{
 		for (int j = 1; j < col; ++j)
-		{
 			result[i][j] = std::min(result[i][j - 1], result[i - 1][j]) + grid[i][j];
-		}
 	}
 
 	return result[row - 1][col - 1];
