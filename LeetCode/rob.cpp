3aaa163d8f7d95afc88de457d7dbb02661ab3e37
LeeNJU <lhb11@software.nju.edu.cn>
2016-03-13 00:25:52 -0500

    house

--- LeetCode/rob.cpp
+++ LeetCode/rob.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include<algorithm>
+#include"TreeNode.h"
 //题目描述：给定一个整数数组代表每个家庭的钱，现在去抢劫，但是不能抢劫相连的两家，求最大能抢劫多少钱
 //解法描述：动态规划  dp[i] = max(dp[i - 1], dp[i - 2] + n[i])
 
@@ -37,4 +38,29 @@ int rob2(std::vector<int>& nums)
 	if (nums.size() <= 1) 
 		return nums.empty() ? 0 : nums[0];
 	return std::max(rob(nums, 0, nums.size() - 1), rob(nums, 1, nums.size()));
+}
+
+
+//变种3:
+//房子被组织成二叉树的形式，父子节点不能同时被抢，求能抢到的最大值
+
+int dfs(TreeNode* root, bool rob)
+{
+	if (root == nullptr)
+		return 0;
+
+	if (rob)
+	{
+		int sum = std::max(dfs(root->left, true), dfs(root->right, true));
+		return std::max(sum, root->val + std::max(dfs(root->left, false), dfs(root->right, false)));
+	}
+	else
+	{
+		return std::max(dfs(root->left, false), dfs(root->right, false));
+	}
+}
+
+int rob(TreeNode* root) 
+{
+	return dfs(root, true);
 }
