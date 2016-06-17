7ddb9d20b398e2d6a56ebfd4c7ca84136f4bcc75
LeeNJU <lihaibonju@gmail.com>
2016-06-16 22:00:08 -0700

    aaa

--- LeetCode/wordBreak.cpp
+++ LeetCode/wordBreak.cpp
@@ -1,6 +1,7 @@
 #include<string>
 #include<vector>
 #include<unordered_set>
+
 //题目描述：给定一个字符串和字符串数组，判断该字符串能否被拆分成字符串数组中的单词，例如s = "leetcode"， dict = "leet", "code"， 那么s能被拆分
 //解法描述：动态规划 转移方程vec[i] = any_of(vec[j]&&s[j + 1; i] 属于 dict); 0 <=j < i
 
