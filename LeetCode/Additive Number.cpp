9f6ccdfe384a63ad2c66807272943238874c352d
LeeNJU <lhb11@software.nju.edu.cn>
2016-08-20 16:37:50 -0400

    Additive Number

--- LeetCode/Additive Number.cpp
+++ LeetCode/Additive Number.cpp
@@ -1,23 +1,33 @@
 #include<string>
 #include<vector>
+//题目描述:给定一个数字字符串，判断其是不是additive number，例如"112358"是additive number，因为可以被分成
+//        "1", "1", "2", "3", "5", "8"并且满足1 + 1 = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8，其中以0开头的
+//        数字字符串是非法字符串，例如"02"
+//解法描述:先枚举第一个数和第二个数，然后两个数相加，递归判断剩下的字符串是否等于两个数之和。
+
+bool isValid(std::string num1, std::string num2, std::string num)
+{   //非法字符串
+	if (num1.size() > 1 && num1[0] == '0' || num2.size() > 1 && num2[0] == '0') 
+		return false;
+	
+	std::string sum = std::to_string(std::stol(num1) + std::stol(num2));
+	if (num == sum) 
+		return true;
+	if (num.size() <= sum.size() || sum.compare(num.substr(0, sum.size())) != 0) 
+		return false;
+	else 
+		return isValid(num2, sum, num.substr(sum.size()));//递归判断
+}
 
 bool isAdditiveNumber(std::string num) 
-{
-	if (num.size() < 3)
-		return false;
-
-	std::vector<bool> dp(num.size(), false);
-	for (int i = 2; i < num.size(); ++i)
+{   //i和j代表第一和第二个数的长度
+	for (int i = 1; i <= num.size() / 2; ++i)//第一个数的长度肯定不会超过字符串的第一半
 	{
-		for (int j = i - 1; j > 0; --j)
+		for (int j = 1; j <= (num.size() - i) / 2; ++j)//第二个数的长度也不会超过剩余字符串的一半
 		{
-			int num1 = stoi(num.substr(0, i + 1));
-			int num2 = stoi(num.substr(i + 1, j - i));
-			for (int k = j + 1; k < num.size(); ++k)
-			{
-				int num3 = stoi(num.substr(j + 1, k - j));
-
-			}
+			if (isValid(num.substr(0, i), num.substr(i, j), num.substr(i + j))) 
+				return true;
 		}
 	}
+	return false;
 }
