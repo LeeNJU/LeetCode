70baefa521b417f6a2a03614a641ea36cddd67c1
LeeNJU <lhb11@software.nju.edu.cn>
2016-08-24 15:36:57 -0400

    change

--- LeetCode/findMin.cpp
+++ LeetCode/findMin.cpp
@@ -3,18 +3,18 @@
 //题目描述：给定一个数组，该数组之前已排好序，但是在某一个点被调换了位置，例如4 5 6 7 0 1 2，找到其中的最小元素，假设数组中不包含重复元素
 //解法描述：二分查找
 
-int findMin(std::vector<int> &num)//将中间的元素与最右边的元素比较，用二分法缩小范围，复杂度为O(lgn)
+int findMin(std::vector<int>& nums)//将中间的元素与最右边的元素比较，用二分法缩小范围，复杂度为O(lgn)
 {
-	int left = 0, right = num.size() - 1, middle = (left + right) / 2;
+	int left = 0, right = nums.size() - 1;
 	while (left < right)
 	{
-		if (num[middle] > num[right])
-			left = middle + 1;
-		else
+		int middle = (left + right) / 2;
+		if (nums[middle] < nums[right])
 			right = middle;
-		middle = (left + right) / 2;
+		else
+			left = middle + 1;
 	}
-	return num[left];
+	return nums[left];
 }
 
 //版本二：假设数组中包含重复元素
