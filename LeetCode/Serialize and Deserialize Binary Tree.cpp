0be987255b236821578716f1511032751b19465e
LeeNJU <lhb11@software.nju.edu.cn>
2016-08-24 19:47:01 -0400

    Serialize and Deserialize Binary Tree

--- LeetCode/Serialize and Deserialize Binary Tree.cpp
+++ LeetCode/Serialize and Deserialize Binary Tree.cpp
@@ -1,44 +1,44 @@
 #include "TreeNode.h"
 #include<string>
-#include<queue>
-#include<vector>
+#include<sstream>
+//题目描述:给定一个二叉树，将其序列化成字符串，然后由字符串反序列化成二叉树
+//解法描述:用先序遍历的方法进行序列化，#代替nullptr,用空格作为每个节点之间的分隔符。反序列化的时候，利用
+//        istringstream来读取每个节点的值，istringstream会一直读取直到遇到空格或者换行符。
+
 class Codec 
 {
+private:
+	TreeNode* deserialize(std::istringstream& in)//这里必须是引用
+	{
+		std::string value;
+		in >> value;
+		if (value == "#")
+			return nullptr;
+		else
+		{
+			TreeNode* root = new TreeNode(stoi(value));
+			root->left = deserialize(in);//读取完左子树的值，in的下一个值就是右子树的值
+			root->right = deserialize(in);
+			return root;
+		}
+	}
 public:
+
+	// Encodes a tree to a single string.
 	std::string serialize(TreeNode* root) 
 	{
-		std::string s;
+		if (root == nullptr)
+			return "# ";
 
-		std::queue<TreeNode*> q;
-		q.push(root);
-		while (!q.empty())
-		{
-			TreeNode* node = q.front();
-			q.pop();
-			if (node == nullptr)
-				s += "nullptr,";
-			else
-			{
-				s += root->val + '0' + ',';
-				q.push(root->left);
-				q.push(root->right);
-			}
-		}
-		return s;
+		std::string left = serialize(root->left);
+		std::string right = serialize(root->right);
+		return std::to_string(root->val) + " " + left + right;
 	}
 
-	TreeNode* deserialize(std::string data)
+	// Decodes your encoded data to tree.
+	TreeNode* deserialize(std::string data) 
 	{
-		std::vector<std::string> vec;
-		for (int i = 0; i < data.size();)
-		{
-			int index = i;
-			while (data[index] != ',')
-				++index;
-			vec.push_back(data.substr(i, index - i));
-			i = index + 1;
-		}
-
-
+		std::istringstream in(data);
+		return deserialize(in);
 	}
 };
