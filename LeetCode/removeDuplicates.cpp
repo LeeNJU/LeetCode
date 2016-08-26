3fad20f37bf471f84f7d5b8dc1138840de4e3b2f
LeeNJU <lhb11@software.nju.edu.cn>
2016-08-26 15:14:23 -0400

    removeduplicates

--- LeetCode/removeDuplicates.cpp
+++ LeetCode/removeDuplicates.cpp
@@ -1,43 +1,42 @@
+#include<vector>
 //题目描述：给一个已序数组，删除其中的重复元素，使得每个元素只出现一次，并返回数组长度
 //解法：index表示新下标，i=1开始遍历数组，如果A[index]不等于A[i]
-int removeDuplicate(int A [], int n)
+int removeDuplicates(std::vector<int>& nums)
 {
-	if (n == 0)
+	if (nums.empty())
 		return 0;
 
 	int index = 0;
-	for (int i = 1; i < n; ++i)//从i=1开始遍历
+	for (int i = 1; i < nums.size(); ++i)
 	{
-		if (A[index] != A[i])
-		{
-			A[++index] = A[i];//注意是++index
-		}
+		if (nums[i] != nums[i - 1])
+			nums[++index] = nums[i];
 	}
+
 	return index + 1;
 }
 
 //版本二：给定一个已序数组，删除其中的重复元素，使得每个元素最多出现两次，并返回长度
 //解法：与上题类似，加一个变量记录次数
-int removeDuplicates(int A [], int n)
+int removeDuplicates2(std::vector<int>& nums) 
 {
-	if (n == 0)
-		return 0;
+	if (nums.size() <= 2)
+		return nums.size();
 
-	int occur = 1, index = 0;
-	for (int i = 1; i < n; ++i)//从i=1开始
+	int count = 1;
+	int index = 0;
+	for (int i = 1; i < nums.size(); ++i)
 	{
-		if (A[index] == A[i])//判断是否相等
+		if (nums[i] == nums[i - 1])
 		{
-			if (occur < 2)//判断出现的次数
-			{
-				A[++index] = A[i];
-				++occur;
-			}
+			++count;
+			if (count <= 2)
+				nums[++index] = nums[i];
 		}
 		else
 		{
-			A[++index] = A[i];
-			occur = 1;
+			nums[++index] = nums[i];
+			count = 1;
 		}
 	}
 	return index + 1;
