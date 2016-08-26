7cd4e7b8009628296f9d58a158dc9adb58718aa0
LeeNJU <lhb11@software.nju.edu.cn>
2016-08-26 14:00:15 -0400

    remove

--- LeetCode/Course Schedule.cpp
+++ LeetCode/Course Schedule.cpp
@@ -39,3 +39,30 @@ bool canFinish(int numCourses, std::vector<std::pair<int, int>>& prerequisites)
 	}
 	return true;
 }
+
+//题目描述：给定一个课程总数n和一些选课条件，返回符合选课条件的一种选课顺序
+//解法描述：拓扑排序
+
+std::vector<int> findOrder(int numCourses, std::vector<std::pair<int, int>>& prerequisites)
+{
+	std::vector<int> result;
+	std::vector<int> indegree(numCourses, 0);//保存每个节点的入度
+	for (int i = 0; i < prerequisites.size(); ++i)
+		++indegree[prerequisites[i].first];
+
+	for (int i = 0; i < numCourses; ++i)
+	{
+		int index = findZeroIndegree(indegree);//找到一个入度为0的节点
+		if (index == -1) //入度为0的节点不存在，直接返回结果
+			return std::vector<int>();
+
+		result.push_back(index);
+		for (int j = 0; j < prerequisites.size(); ++j)
+		{
+			if (prerequisites[j].second == index)
+				--indegree[prerequisites[j].first];
+		}
+		indegree[index] = -1;
+	}
+	return result;
+}
