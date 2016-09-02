d15acb00ac97031a4a6b134ba10a00a47629e0eb
LeeNJU <lhb11@software.nju.edu.cn>
2016-09-02 17:39:10 -0400

    change

--- LeetCode/Palindrome Permutation.cpp
+++ LeetCode/Palindrome Permutation.cpp
@@ -4,13 +4,12 @@
 //解法描述:如果能够组成回文串，那么字符出现的次数为偶数，最多只能有一个字符的出现次数为奇数
 bool canPermutePalindrome(std::string s)
 {
-	int dict[256] = { 0 };
 	std::vector<int> vec(256, 0);
 	for (int i = 0; i < s.size(); ++i)//统计每个字符的次数
 		++vec[s[i]];
 
 	int count = 0;
-	for (int i = 0; i<256; i++)//统计奇数字符的个数
+	for (int i = 0; i < vec.size(); ++i)//统计奇数字符的个数
 	{
 		if (vec[i] % 2 != 0)   
 			++count;
