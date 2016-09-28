71c4e9d6640257350bbb9c5beb252862119a8dd7
LeeNJU <lhb11@software.nju.edu.cn>
2016-09-28 14:19:29 -0400

    Increasing Triplet Subsequence

--- LeetCode/Increasing Triplet Subsequence.cpp
+++ LeetCode/Increasing Triplet Subsequence.cpp
@@ -1,17 +1,19 @@
 #include<vector>
 #include<algorithm>
-
-bool increasingTriplet(std::vector<int>& nums) 
+//题目描述:给定一个未排序的数组，判断是否存在一个长度为3递增的子序列，例如
+//         num[i] < num[j] < num[k]并且i < j < k
+//解法描述:遍历数组，保存第一个最小值和倒数第二小的值，如果当前元素比最小值
+//         和倒数第二小的值都小，说明此时有3个递增的子序列了，返回true
+bool increasingTriplet(vector<int>& nums) 
 {
-	std::vector<int> dp(nums.size(), 1);
-	for(int i = 1; i < nums.size(); ++i)
+	int m1 = INT_MAX, m2 = INT_MAX;
+	for (auto a : nums) 
 	{
-		for(int j = 0; j < i; ++j)
-		{
-			if(nums[i] > nums[j])
-				dp[i] = std::max(dp[i], dp[j] + 1);
-		}
-		if(dp[i] >= 3)
+		if (m1 >= a) 
+			m1 = a;
+		else if (m2 >= a) 
+			m2 = a;
+		else 
 			return true;
 	}
 	return false;
