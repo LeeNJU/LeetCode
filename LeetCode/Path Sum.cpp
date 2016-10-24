f4a38d749f2072b65b11219c5226cf942da6063b
LeeNJU <lhb11@software.nju.edu.cn>
2016-10-24 18:37:49 -0400

    Path Sum

--- LeetCode/Path Sum.cpp
+++ LeetCode/Path Sum.cpp
@@ -33,3 +33,29 @@ void dfs(TreeNode* root, int sum, std::vector<std::vector<int>>& result, std::ve
 	dfs(root->left, sum - root->val, result, temp);//左节点递归
 	dfs(root->right, sum - root->val, result, temp);//右节点递归
 }
+
+//题目描述:给定一个二叉树，找到和等于某个值的路径的个数，路径不一定要从root开始，也不一定要到叶子节点结束，
+//        但是必须是从父节点到子节点的路径
+
+int pathSumRec(TreeNode* root, int sum) //以root为起点，找到和为sum的路径个数
+{
+	if (root == nullptr)
+		return 0;
+
+	int rootmatch = (root->val == sum);
+	int left = pathSumRec(root->left, sum - root->val);
+	int right = pathSumRec(root->right, sum - root->val);
+
+	return rootmatch + left + right;
+}
+
+int pathSum(TreeNode* root, int sum) 
+{
+	if (root == nullptr)
+		return 0;
+
+	int rootval = pathSumRec(root, sum);
+	int lefttree = pathSum(root->left, sum);
+	int righttree = pathSum(root->right, sum);
+	return (rootval + lefttree + righttree);
+}
