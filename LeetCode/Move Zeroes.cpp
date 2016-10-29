4d70939e7a3ab227578648a26657e29bd21c2e4b
LeeNJU <lhb11@software.nju.edu.cn>
2016-10-29 15:39:36 -0400

    move zero

--- LeetCode/Move Zeroes.cpp
+++ LeetCode/Move Zeroes.cpp
@@ -3,18 +3,9 @@
 //解法描述：遍历数组，碰到一个0，就找到后序元素中的非0元素并与之交换
 void moveZeroes(std::vector<int>& nums) 
 {
-	for (int i = 0; i < nums.size(); ++i)
+	for (int i = -1, j = 0; j < nums.size(); ++j)
 	{
-		if (nums[i] == 0)
-		{
-			int index = i + 1;
-			while (index < nums.size() && nums[index] == 0) //寻找后序元素中的非0元素
-				++index;
-
-			if (index == nums.size()) //后序元素中没有非0元素，说明已经完成操作
-				return;
-			else
-				std::swap(nums[i], nums[index]);
-		}
+		if (nums[j] != 0)
+			std::swap(nums[++i], nums[j]);
 	}
 }
