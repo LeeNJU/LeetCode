cbb4201d04719be75a0d63ee97abdd3a08eda3db
LeeNJU <lhb11@software.nju.edu.cn>
2016-10-30 19:38:38 -0400

    Permutation Index II

--- LeetCode/Permutation Index.cpp
+++ LeetCode/Permutation Index.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include<unordered_map>
 //题目描述:给定一个整数数组，求出permutation index，例如[1,2,4]，返回1，[1,4,2],返回2
 //解法描述:确定第一个元素，那么剩下的有(n - 1)!中组合方式
 long long permutationIndex(std::vector<int>& A) 
@@ -25,4 +26,37 @@ long long permutationIndex(std::vector<int>& A)
 	}
 
 	return result + 1;
+}
+
+//如果包含重复元素呢，求permutation index
+//解法描述:给定n个数，包含重复数字，这n个数的排列组合数是n!/(A1! * A2! * A3! * ... * An!),A1表示第一个数的
+//        出现次数，A2表示第二个数的出现次数。与上一题不同的是要记录元素出现的次数，然后相除。从低位到高位，
+//        
+long long permutationIndexII(std::vector<int>& A) 
+{
+	if (A.empty()) 
+		return 0;
+
+	int n = A.size();
+	std::unordered_map<int, int> map;
+
+	long index = 0, fact = 1, mulFact = 1;
+	for (int i = n - 1; i >= 0; --i) 
+	{
+		++map[A[i]];
+		if(map[A[i]] > 1) 
+			mulFact *= map[A[i]];//mulFact就是(A1! * A2! * A3!...* An!)
+	
+		int count = 0;
+		for (int j = i + 1; j < n; ++j) //比当前数字小的个数
+		{
+			if (A[i] > A[j])
+				++count;
+		}
+
+		index += count * fact / mulFact;//fact就是阶乘
+		fact *= (n - i);
+	}
+	
+	return index + 1;
 }
--- LeetCode/k Sum II.cpp
+++ LeetCode/k Sum II.cpp
@@ -2,31 +2,31 @@
 #include<algorithm>
 //题目描述:给定一个unique的数组和一个k和一个target，要求找到所有k个元素的组合，使得它们的和等于target
 //解法描述:就是k sum，先排序，再用递归
-void dfs(int index, int depth, int k, std::vector<int>& num, std::vector<std::vector<int>>&result, std::vector<int> intermediate, int sum, int target)
+void dfs(int index, int k, std::vector<int>& num, std::vector<std::vector<int>>&result, 
+	std::vector<int> intermediate, int target)
 {
-	if (depth == k) //当前递归深度
+	if (k == 0)
 	{
-		if (sum == target) //sum是intermediate里面元素的和
+		if (target == 0)
 			result.push_back(intermediate);
 		return;
 	}
 	for (int i = index; i < num.size(); ++i)
 	{
-		if (sum + num[i] > target)
-			continue;
+		if (target < num[i])
+			break;
 
 		intermediate.push_back(num[i]);
-		dfs(i + 1, depth + 1, k, num, result, intermediate, sum + num[i], target);
+		dfs(i + 1, k - 1, num, result, intermediate, target - num[i]);
 		intermediate.pop_back();
 	}
 }
 
-
-std::vector<std::vector<int> > kSumII(std::vector<int> A, int k, int target)
+std::vector<std::vector<int> > kSumII(std::vector<int> A, int k, int target) 
 {
 	sort(A.begin(), A.end());
 	std::vector<std::vector<int>> result;
 	std::vector<int> intermediate;
-	dfs(0, 0, k, A, result, intermediate, 0, target);
+	dfs(0, k, A, result, intermediate, target);
 	return result;
 }
