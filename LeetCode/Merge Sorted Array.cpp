ea9070afa25b4eb7815f71aa1cab340d2107373d
LeeNJU <lhb11@software.nju.edu.cn>
2017-03-01 19:35:28 -0500

    merge

--- LeetCode/Merge Sorted Array.cpp
+++ LeetCode/Merge Sorted Array.cpp
@@ -1,20 +1,21 @@
-#include<iostream>
+#include<vector>
 #include<algorithm>
 //题目描述：给定两个已序数组A和B，将其合并,假设数组A后面有足够的空间容纳数组B
 //解法描述：从A和B的结尾开始扫描，依次将值较大的放到数组A的最后面，最后把数组B剩下的内容拷贝到数组A中
 
-void mergeSortedArray(int* a, int lenA, int* b, int lenB)
+void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
 {
-	if (a == nullptr || b == nullptr)
-		return;
-
-	int indexA = lenA - 1, indexB = lenB - 1, cur = lenA + lenB - 1;
-
-	while (indexA >= 0 && indexB >= 0)
+	int index = m + n - 1;
+	int i = m - 1, j = n - 1;
+	while (i >= 0 && j >= 0)
 	{
-		a[cur--] = a[indexA] <= b[indexB] ? b[indexB--] : a[indexA--];//依次将值较大的放到数组A的后面
+		if (nums1[i] >= nums2[j])
+			nums1[index--] = nums1[i--];
+		else
+			nums1[index--] = nums2[j--];
 	}
 
-	std::copy(b, b + indexB + 1, a);//把B剩下的复制到A前面
+	if (i < 0)
+		copy(nums2.begin(), nums2.begin() + j + 1, nums1.begin());
 }
 
